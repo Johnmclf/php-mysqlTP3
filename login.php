2dd4d5d99f1a68d021ba1924d173868d6f7a783e
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-09-17 11:02:30 +0200

    Implémentez un système de connexion

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
  // Validation du formulaire
  if (isset($_POST['email']) && isset($_POST['password'])) {
     foreach ($users as $user) {
-        if ( ... ) {
+        if ( $user['email'] === $_POST['email'] && $user['password'] === $_POST['password']) {
             $loggedUser = [ 'email' => $user['email'], ];
         }
         else {
@@ -14,26 +14,25 @@
  ?>
 
  <!-- Si utilisateur/trice est non identifié(e), on affiche le formulaire -->
- <?php if ( ... ) : ?>
- <form action = ... method = ...>
+ <?php if ( !isset($loggedUser) ) : ?>
+ <form action ="" method = POST>
  <!-- si message d'erreur on l'affiche -->
- <?php if ( ... ) : ?>
+ <?php if ( isset($errorMessage) ) : ?>
  <div class="alert alert-danger" role="alert">
 
- <?php echo ... ; ?>
+ <?php echo $errorMessage; ?>
  </div>
  <?php endif; ?>
  <div class="mb-3">
  <label for="email" class="form-label">Email</label>
- <input type="email" class="form-control" id="email" name= "..."
- aria-describedby="email-help" placeholder="you@exemple.com">
+ <input type="email" class="form-control" id="email" name= "email" aria-describedby="email-help" placeholder="you@exemple.com">
  <div id="email-help" class="form-text">
  L'email utilisé lors de la création de compte.
  </div>
  </div>
  <div class="mb-3">
  <label for="password" class="form-label">Mot de passe</label>
- <input type="password" class="form-control" id="password" name= "..." >
+ <input type="password" class="form-control" id="password" name= "password" >
  </div>
  <button type="submit" class="btn btn-primary">Envoyer</button>
  </form>
